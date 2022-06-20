180841cff2a5fdbe1345c2bdace29813c3bcc194
Bo Fethe <bfethe@gmail.com>
2022-06-20 03:49:02 -0400

    adding attachment exporter

---
+++ AGOL_AttachmentExporter.py
@@ -0,0 +1,32 @@
+import arcpy
+from arcpy import da
+import os
+
+# gdb attachment table path
+gdbpath = input('Enter the path to the geodatabase containing the attachments: ')
+fcPath = gdbpath
+inTable = gdbpath + '__ATTACH'
+
+# output folder path
+outputFolder = input('Enter the folder path to save the attachments in: ')
+
+
+# Names of fields you want the attachment to be
+field1 = input('Enter the attribute field name containing the desired filenames: ')
+
+with da.SearchCursor(fcPath, ['OBJECTID', field1]) as fc:
+    nameDic = {}
+    for entry in fc:
+        nameDic[entry[0]] = str(entry[1])
+
+with da.SearchCursor(inTable, ['DATA', 'REL_OBJECTID']) as cursor:
+    outputList = []
+    for item in cursor:
+        attachment = item[0]
+        filename = nameDic[item[1]]
+        while filename in outputList:
+            filename = filename + '_'
+
+        open(outputFolder + os.sep + filename + '.jpg', 'wb').write(attachment.tobytes())
+        outputList.append(filename)
+        print('Exported: ', filename)
