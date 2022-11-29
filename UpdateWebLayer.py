b35ba9f18141d3e19d1127e8b56a7681f6ed4df8
Bo Fethe <bfethe@gmail.com>
2022-11-29 15:34:22 -0500

    new fields creation automated

--- UpdateWebLayer.py
+++ UpdateWebLayer.py
@@ -2,7 +2,7 @@ import arcgis
 import arcpy
 
 ### TODO Define the web layer to update ###
-webLayerURL = 'https://services1.arcgis.com/O1JpcwDW8sjYuddV/arcgis/rest/services/Test5/FeatureServer/0'
+webLayerURL = 'https://services.arcgis.com/rD2ylXRs80UroD90/arcgis/rest/services/test5/FeatureServer/0'
 newLayerPath = r'C:\Users\bfethe\Desktop\Temp\GIS\Test\e2357855-bace-4c36-95bb-b6d9039ada43.gdb\Test'
 
 # Initiate the gis object
@@ -13,6 +13,24 @@ gis = arcgis.GIS('Pro')
 print('Truncating layer...')
 arcpy.management.TruncateTable(webLayerURL)
 
+# Add missing fields from local layer to web layer, if applicable
+print('Checking for new fields to add to web layer')
+webFields = arcpy.ListFields(webLayerURL)
+newFields = arcpy.ListFields(newLayerPath)
+
+webFieldNames = []
+for f in webFields:
+    webFieldNames.append(f.name)
+
+for f in newFields:
+    if f.name not in webFieldNames and f.type != 'Geometry':
+        print('\tAdding field [{}: {}] to web layer'.format(f.name, f.type))
+        arcpy.management.AddField(in_table=webLayerURL,
+                                    field_name=f.name,
+                                    field_type=f.type)
+
 # Append new data to original web layer
 print('Appending new data to web layer...')
-arcpy.management.Append(newLayerPath, webLayerURL, schema_type='TEST')
+arcpy.management.Append(newLayerPath, webLayerURL, schema_type='NO_TEST')
+
+print('\nDone!')
